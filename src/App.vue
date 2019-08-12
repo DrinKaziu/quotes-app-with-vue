d8d90ad901f00728b4e7a38b3bbf15a21b04f0c5
SnowSurfer <drinkaziu@gmail.com>
2019-08-12 12:53:55 -0400

    add status bar and max quotes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @addQuote="newQuote"></app-new-quote>
     <app-quotes-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quotes-grid>
     <div class="row">
@@ -13,6 +14,7 @@
 <script>
   import QuotesGrid from './components/QuotesGrid.vue';
   import NewQuote from './components/NewQuote.vue';
+  import Header from './components/Header.vue';
   export default {
     data: function() {
       return {
@@ -22,6 +24,9 @@
     },
     methods: {
       newQuote(quote) {
+        if (this.quotes.length >= this.maxQuotes) {
+          return alert("Maximum number of quotes reached. Please delete some quotes in order to add new ones!")
+        }
         this.quotes.push(quote)
       },
       deleteQuote(index) {
@@ -30,7 +35,8 @@
     },
     components: {
       appQuotesGrid: QuotesGrid,
-      appNewQuote: NewQuote
+      appNewQuote: NewQuote,
+      appHeader: Header
     }
   }
 </script>
