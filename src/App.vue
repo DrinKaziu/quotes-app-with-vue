63187b76f38c10e1c024d510be93c103d2110373
SnowSurfer <drinkaziu@gmail.com>
2019-08-12 12:31:52 -0400

    add create quote functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
+    <app-new-quote @addQuote="newQuote"></app-new-quote>
     <app-quotes-grid :quotes="quotes"></app-quotes-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">info: Click on Quote to Delete it</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import QuotesGrid from './components/QuotesGrid.vue'
+  import QuotesGrid from './components/QuotesGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
   export default {
     data: function() {
       return {
@@ -13,8 +20,14 @@
         maxQuotes: 10
       }
     },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote)
+      }
+    },
     components: {
-      appQuotesGrid: QuotesGrid
+      appQuotesGrid: QuotesGrid,
+      appNewQuote: NewQuote
     }
   }
 </script>
